ca85a9b5437f337f512f73fc91066e62507cf41e
reidipv-code <reidipv@gmail.com>
2026-09-13 18:12:03 -0400

    Update sesiones.py

--- core/sesiones.py
+++ core/sesiones.py
@@ -2,7 +2,7 @@ import sqlite3
 import os
 import time
 
-DB_PATH = os.path.expanduser("~/telegram_bot/usuarios.db")
+DB_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "usuarios.db")
 
 
 def init_sesiones_db():

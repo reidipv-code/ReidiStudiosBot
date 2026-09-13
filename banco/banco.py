934725dfa7093fbd27f8840af9ca75abad315b53
reidipv-code <reidipv@gmail.com>
2026-09-13 18:10:46 -0400

    Update banco.py

--- banco/banco.py
+++ banco/banco.py
@@ -5,7 +5,7 @@ from telegram.ext import ContextTypes
 
 from core.db import obtener_datos, actualizar_tokens
 
-DB_PATH = os.path.expanduser("~/telegram_bot/usuarios.db")
+DB_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "usuarios.db")
 
 LIMITE_BANCO = 4000
 

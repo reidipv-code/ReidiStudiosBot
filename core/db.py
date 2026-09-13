0f996f18ea97e2213a4aa3b985639229c46b27ca
reidipv-code <reidipv@gmail.com>
2026-09-13 18:11:26 -0400

    Update db.py

--- core/db.py
+++ core/db.py
@@ -1,7 +1,7 @@
 import sqlite3
 import os
 
-DB_PATH = os.path.expanduser("~/telegram_bot/usuarios.db")
+DB_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "usuarios.db")
 
 
 def init_db():

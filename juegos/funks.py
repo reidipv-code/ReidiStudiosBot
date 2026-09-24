b59608755b38db5a09913b3194238c060c7f0d4e
reidipv-code <reidipv@gmail.com>
2026-09-24 07:55:28 -0400

    Update funks.py

--- juegos/funks.py
+++ juegos/funks.py
@@ -9,8 +9,8 @@ from core.sesiones import iniciar_partida, terminar_partida
 from core.misiones import sumar_progreso
 from core.tienda import aplicar_bonus_xp, cantidad_con_bonus_tokens
 
-COOLDOWN_PERDIDA = 420
-COOLDOWN_VICTORIA = 600
+COOLDOWN_PERDIDA = 240
+COOLDOWN_VICTORIA = 195
 TIEMPO = 40
 CANCIONES_POR_PARTIDA = 3
 
